118447e2fbca9eb5fe9df2b4e133798a27abb183
Bin Liu <binandmolly@outlook.com>
2026-08-04 17:08:47 +0800

    fix: refresh ARC TODO client cache on release

--- public/arc-todo/sw.js
+++ public/arc-todo/sw.js
@@ -1,7 +1,7 @@
-const CACHE = "arc-todo-v2";
+const CACHE = "arc-todo-v3";
 const ASSETS = ["/arc-todo/", "/arc-todo/index.html", "/arc-todo/styles.css", "/arc-todo/app.js", "/arc-todo/manifest.webmanifest", "/arc-todo/icon.svg"];
-self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
-self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((names) => Promise.all(names.filter((name) => name !== CACHE).map((name) => caches.delete(name)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
